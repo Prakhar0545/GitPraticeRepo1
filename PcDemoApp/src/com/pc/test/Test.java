4d78841720e22eaaa68e861bc2e49ccb62d6fbba
#Pc_WorldLife <#Pc_WorldLife@DESKTOP-IK0R0EE>
2020-04-19 16:42:34 +0530

    updated by TL Test.java

--- PcDemoApp/src/com/pc/test/Test.java
+++ PcDemoApp/src/com/pc/test/Test.java
@@ -1,5 +1,11 @@
 package com.pc.test;
 
+import com.pc.domain.Select;
+
 public class Test {
+	public static void main(String[] args) {
+		Select s=new Select();
+		System.out.println(s.getClass());
+	}
 
 }
